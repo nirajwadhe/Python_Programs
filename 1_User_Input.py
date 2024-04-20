cd0b2e4f77637df4209ede57a6e21ed868b425e2
nirajwadhe <nirajwadhe@gmail.com>
2024-04-20 10:45:36 +0530

    [Niraj] Add : Python Script to check length of string

---
+++ 1_User_Input.py
@@ -0,0 +1,29 @@
+'''
+Take User Name as Input. Ensure UserName has min 3 char
+Logic -> Replace <<UserName>> with the proper name
+O/P -> Print the String with User Name
+'''
+
+def main():
+    username = input("Enter a username - ")
+    try:
+        if len(username) > 3  :
+            print(f"Hello {username}, How are you?")    
+        else :
+            print(len(username)/0)
+    except :    
+        main()
+
+# def main():
+#     username = input("Enter a Username - ")
+#     if len(username) > 3 :
+#         print(f"Hello {username}, How are you?") 
+#     else :
+#         main()    
+
+if __name__ == "__main__":
+    main()
+
+
+                
+
