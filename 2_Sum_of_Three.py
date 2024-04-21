7aa5647d4d750921342f02c351bd84df20e02a53
nirajwadhe <nirajwadhe@gmail.com>
2024-04-21 12:20:41 +0530

    [Niraj] Refract : Correction in code made list_of_num global

--- 2_Sum_of_Three.py
+++ 2_Sum_of_Three.py
@@ -1,10 +1,11 @@
 def main():
+    global list_of_num
     list_of_num = []
     try :
         number = int(input("Enter a Number - "))
         while number!=0:
-            number = int(input("Enter a Number -"))
             list_of_num.append(number)
+            number = int(input("Enter a Number -"))
     except Exception as e:
         print(e)
     three_sum(list_of_num)
@@ -12,7 +13,7 @@ def main():
 def three_sum(list_num):
     for i in range(len(list_num)):
         for j in range(i+1,len(list_num)):
-            for k in range(j+2,len(list_num)):
+            for k in range(j+1,len(list_num)):
                 if list_num[i]+list_num[j]+list_num[k]==0 :
                     print(f'Yes {list_num[i]},{list_num[j]},{list_num[k]} are triplets')
 
