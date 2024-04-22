604e5b796266fa014e6b335f985343691f7c1d72
nirajwadhe <nirajwadhe@gmail.com>
2024-04-22 10:50:39 +0530

    [Niraj] Add : Python script to create a 2 D array

--- 1_2D_Array.py
+++ 1_2D_Array.py
@@ -1,5 +1,22 @@
+import numpy as np
+import random as rm
 def main():
-    pass
+    elements = []
+    try :
+        M = int(input("Enter number of Rows -"))
+        N = int(input("Enter numner of Columns -"))
+    except Exception as e :
+        print(e)
+        main()
+
+    for i in range(0,M*N):
+        elements.append(rm.randrange(-100,100))
+
+    print(reshape_array(elements,M,N))
+
+def reshape_array(element,rows,columns):
+    arr = np.array(element)
+    return arr.reshape(rows,columns)
 
 if __name__=="__main__":
     main()
